391f85415337a6ca9665becd5f8a0af073860dbe
lxg <1670151546@qq.com>
2018-12-19 20:03:44 +0800

    none

---
+++ src/views/stock/BrandDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+import myheader from '@/components/'
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    methods:{},
+    components:{}
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
+
