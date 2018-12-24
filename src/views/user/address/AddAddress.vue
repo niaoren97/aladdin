52232e914de497d5df5e5b46611a2e3813e14d44
lxg <1670151546@qq.com>
2018-12-24 15:45:58 +0800

    地址三级联动

--- src/views/user/address/AddAddress.vue
+++ src/views/user/address/AddAddress.vue
@@ -4,38 +4,66 @@
     template(slot="right")
       span(@click="submit") 完成
   .content
-    .warn 请使用真实姓名
-    section-block
-      input(v-model="name")
     list
       list-item
+        |姓名：
+        input(v-model="name")
+      list-item
+        | 电话：
         input(v-model="phone")
       list-item
-        input(v-model="address")
+        |地址：
+        City(v-model="cityInfo", class="select-city" ,           
+        @input="chooseAddress")
       list-item
+        |详细：
         input(v-model="detail")
-      list-item
-        input(v-model="zipCode")
 </template>
 <script>
+import City from "@/components/cart/City";
 export default {
   name: "AddAddress",
+  components: {
+    City
+  },
   data() {
     return {
-      name: '',
-      phone: '',
-      address: '',
-      detail: '',
-      zipCode: '',
-    }
+      name: "",
+      phone: "",
+      address: "",
+      detail: "",
+      cityInfo: "",
+    };
   },
   methods: {
-    submit() {
-      // FIXME:
-      this.$nivagitor.pop()
+    // info 为子组件传递过来的参数
+    chooseAddress(info) {
+      console.log("info", info);
+      this.cityInfo = info;
+    },
+    submit() {}
+  },
+  computed: {
+    //cityName 为选择的省市区名称
+    cityName() {
+      const names = [];
+      this.cityInfo.province && names.push(this.cityInfo.province.name + " ");
+      this.cityInfo.city && names.push(this.cityInfo.city.name + " ");
+      this.cityInfo.block && names.push(this.cityInfo.block.name + " ");
+      return names.join("");
     }
   }
-}
+};
 </script>
 <style lang="stylus" scoped>
+.single-page
+  background-color #fff
+
+.conten
+  width 100%
+
+.warn
+  font-size 20px
+  color #ff0000
+  padding 5px
 </style>
