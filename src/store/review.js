2747298ab3584540a21266e7bcf69b4c2a971013
lxg <1670151546@qq.com>
2018-12-25 18:04:20 +0800

    0

--- src/store/review.js
+++ src/store/review.js
@@ -1,41 +1,33 @@
-import Vue from 'vue'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
 
 export default {
   namespaced: true,
   state: {
-    reviews: {
-      // 1: {
-      //   id: 1,
-      //   pic: '',
-      //   ni: '',
-      //   comment: '',
-      //   picture: '',
-      // },
-    },
+    reviews: {}
   },
   mutations: {
     addReview(state, reviews) {
-      reviews.forEach((review) => {
-        Vue.set(state.reviews, review.id, review)
-      })
-    },
+      reviews.forEach(review => {
+        Vue.set(state.reviews, review.id, review);
+      });
+    }
   },
   actions: {
     fetchReview({ commit, rootState }) {
-      const token = rootState.user.me.token
+      const token = rootState.user.me.token;
       axios
         .post(
           '/api/v1/user/review',
           {},
           {
-            headers: { Authorization: `Bearer ${token}` },
+            headers: { Authorization: `Bearer ${token}` }
           }
         )
-        .then((res) => {
-          commit('addReview', res.data)
-          commit('user/addReview', res.data.map((r) => r.id), { root: true })
-        })
-    },
-  },
-}
+        .then(res => {
+          commit('addReview', res.data);
+          commit('user/addReview', res.data.map(r => r.id), { root: true });
+        });
+    }
+  }
+};
