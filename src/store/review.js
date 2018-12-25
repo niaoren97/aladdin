34cb2fd6998fa35d0ea99634cdd02c99272f01cc
lxg <1670151546@qq.com>
2018-12-25 14:09:59 +0800

    12.25

---
+++ src/store/review.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+export default {
+  namespace: true,
+  state: {
+    items:{
+      1: {
+        id: 1,
+        pic:'',
+        ni: '',
+        comment: '',
+        picture: ''
+      }
+    }
+  },
+  mutation: {
+
+  },
+  action: {
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
